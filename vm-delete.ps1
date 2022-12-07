f9dbc46836220e97d86714d119141534cb9c1cb2
FoolApe <a22951701a@gmail.com>
2022-12-07 16:50:34 +0800

    Add two VM's scripts.

---
+++ vm-delete.ps1
@@ -0,0 +1,63 @@
+### First version: 2022/12/02
+### Create by: SI-SEP-Juro
+### Description: To delete multiple VMs at once.
+
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# !!! THIS SCRIPT IS DANGEROUS AS FUCK !!!!
+# !!!        PLEASE BE CAREFUL		   !!!!	
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+$user='username@vsphere.local' ### Your VC account that has privileges to delete/poweroff vms.
+$passwd=cat /Path/to/your/password
+$server='Your_vCenter_IP'
+
+
+echo " "
+echo "---------------"
+echo "|Connecting VC|"
+echo "---------------"
+Connect-VIServer -Server $server -User $user -Password $passwd
+echo " "
+
+#$hostname = "/Path/to/your/VM/ip_list"
+$log = '/Path/to/your/log'
+
+' '
+'------------------------------- Starting Shutdown VM -------------------------------'
+echo "" >> $log
+echo "===== SHUT =====" >> $log
+Foreach($a in get-content $hostname)
+{
+	$name = echo $a |awk '{print $1}'
+	$ip = echo $a |awk '{print $2}'
+	
+	### Check vm ip then power-off them.
+	get-vm -name $name |where {$_.PowerState -match 'PoweredOn' -and  $_.guest.IPAddress[0] -eq "$ip"} |Stop-VM -Confirm:$False
+	echo "$name,$ip 關機" >> $log 
+}
+
+
+### Wait for shutting down
+Start-Sleep -Seconds 5
+
+'------------------------------- Starting Delete VM -------------------------------'
+echo "" >> $log
+echo "===== DELE =====" >> $log
+Foreach($b in get-content $hostname)
+{
+	$name = echo $b |awk '{print $1}'
+    $ip = echo $b |awk '{print $2}'	
+
+	### Delete VMs after they are power-off.
+	get-vm -name $name |where {$_.PowerState -match 'PoweredOff'} |Remove-VM -Confirm:$False
+	echo "$name,$ip 刪除" >> $log	
+}
+echo "======== DONE ========" >> $log
+echo "" 
+Disconnect-VIServer -Server $server -Confirm:$false
+echo "--------------"
+echo "|Process done|"
+echo "--------------"
+echo " "
+
---
+++ vm-vmotion.ps1
@@ -0,0 +1,67 @@
+### Description: To vMotion multiple VMs.
+
+
+$user='username@vsphere.local' ### Your VC account that has privileges to vMotion/create jobs.
+$passwd=cat /Path/to/your/password
+$server='Your_VC_IP'
+
+echo " "
+echo "---------------"
+echo "|Connecting VC|"
+echo "---------------"
+Connect-VIServer -Server $server -User $user -Password $passwd
+echo " "
+
+### Set-up cluster spec
+$spec = New-Object VMware.Vim.VirtualMachineRelocateSpec
+$cluster=Get-Cluster -Name Cluster_name ### Destnation cluster
+$rp=Get-ResourcePool -Location (Get-Cluster -Name $cluster) ### Destnation resource-pool
+$spec.Pool = $rp.ExtensionData.MoRef
+
+
+$VMlist = "/Path/to/your/vmotion_list" ### Use VM hostname as content.
+'------------------------------- Starting Move VM -------------------------------'
+Foreach($a in get-content $VMlist)
+{
+	### Set-up VM spec
+	$vm = Get-VM -Name $a
+	#$vm_ds=Get-VM -Name $a |Get-Datastore |awk '{print $1}' |egrep "C1|C2"
+	$vm_ds=Get-VM -Name $a |Get-Datastore -Name "DS_name" ### VM source volume
+	$ds=Get-Datastore -Location (Get-Datacenter -Name DC_nmae) -Name $vm_ds
+	$ds_chk=echo $ds |awk -F \_ '{print $1}'
+	$spec.Datastore = $ds.ExtensionData.MoRef
+
+	$esx=Get-VMHost -Name Host_name ### Destination host
+    $spec.Host = $esx.ExtensionData.MoRef
+
+	echo "*** $a ***"
+	$vm.ExtensionData.RelocateVM($spec,"defaultPriority") ### vMotion job
+	echo ""
+}
+
+
+Disconnect-VIServer -Server $server -Confirm:$false
+echo "--------------"
+echo "|Process done|"
+echo "--------------"
+echo " "
+
+
+
+### 正確搬移方法sample ###
+
+#$vmName = "TestVM"
+#$tgtDatastore = "datastore"
+#$tgtCluster = "cluster"
+#$tgtPool = "resourcepool"
+#
+#$vm = Get-VM -Name $vmName
+#$ds = Get-Datastore -Name $tgtDatastore
+#$esx = Get-Cluster -Name $tgtCluster | Get-VMHost | Get-Random
+#$rp = Get-ResourcePool -Name $tgtPool
+#
+#$spec = New-Object VMware.Vim.VirtualMachineRelocateSpec
+#$spec.Datastore = $ds.ExtensionData.MoRef
+#$spec.Host = $esx.ExtensionData.MoRef
+#$spec.Pool = $rp.ExtensionData.MoRef
+#$vm.ExtensionData.RelocateVM($spec,"defaultPriority")
